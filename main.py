1d19b7179b41ee883513a3b44203bc001f1d2502
Roi Yehoshua <r.yehoshua@northeastern.edu>
2021-09-23 11:58:40 -0400

    Adding the main script that runs the game

---
+++ main.py
@@ -0,0 +1,12 @@
+print('Welcome to the great game of reversi')
+print('************************************')
+
+mode = int(input('Choose game mode (1-3): '))
+if mode == 1:
+    print("You've chosen to play against a local opponent")
+elif mode == 2:
+    print("You've chosen to play an AI agent")
+elif mode == 3:
+    print("You've chosen to play online")
+else:
+    print("You've entered an invalid option")
